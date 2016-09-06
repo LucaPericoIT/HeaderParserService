431a26cfa95f81869fc6f59e8ac8277204053141
Luca Perico <luca.perico90@gmail.com>
2016-09-06 19:07:36 +0000

    Update gitignore

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var http = require('http')
 var parser = require('./headerparsermodule');
 
 var server = http.createServer(function(request, response) {
-	console.log(request.headers);
-	var r = parser.parseHeaders(request.headers);
+
+	// Parse headers
+	var res = parser.parseHeaders(request.headers);
+	
+	// Send response
 	response.setHeader('Content-Type', 'application/json');
-	response.end(JSON.stringify(r));
+	response.end(JSON.stringify(res));
 });
 
 server.listen(8080);
